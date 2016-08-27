d807a7dfa166d746a8afad854a390008720fcde6
Sam Beckmann <samvbeckmann@gmail.com>
2016-08-27 18:07:11 -0500

    Started adding a symbol table and id_res machine - Not yet working

--- src/machines.c
+++ src/machines.c
@@ -1,8 +1,7 @@
 #include "machines.h"
 #include "word_defs.h"
 #include <string.h>
-
-#include <stdio.h>
+#include <ctype.h>
 
 static union Optional_Token make_optional(
                         char lexeme[],
@@ -14,15 +13,40 @@ static union Optional_Token make_optional(
         token.token_type = type;
         token.attribute.attribute = attr;
         token.forward = forward;
-        union Optional_Token op_token;
-        op_token.token = token;
-        return op_token;
+        return wrap_token(token);
 }
 
 static union Optional_Token null_optional() {
         union Optional_Token op_token;
         op_token.nil = NULL;
         return op_token;
+}
+
+static union OptionalToken wrap_token(struct Token token)
+{
+        union Optional_Token op_token;
+        op_token.token = token;
+        return op_token;
+}
+
+static struct Symbol * add_symbol(char word[], struct Symbol *sym_table)
+{
+        if (*sym_table == NULL) {
+                struct Symbol symbol;
+                strcpy(symbol.word, word);
+                symbol.ptr = NULL;
+                return &symbol;
+        } else if (strcmp(sym_table -> word, word) == 0) {
+                return sym_table -> ptr;
+        } else {
+                return add_symbol(word, sym_tableptr);
+        }
+}
+
+static union Optional_Token check_reserved_words(char word[])
+{
+        // TODO: Implement reserved words
+        return null_optional();
 }
 
 union Optional_Token relop_machine(char *forward, char *back)
@@ -70,9 +94,37 @@ union Optional_Token int_machine(char *forward, char *back)
         return null_optional();
 }
 
-union Optional_Token id_res_machine(char *forward, char *back)
+union Optional_Token id_res_machine(char *forward, struct Symbol *sym_table)
 {
-        return null_optional();
+        char word[11];
+        int i = 0;
+        char value = *forward++;
+        while (isalnum(value)) {
+                if (i < 10)
+                        word[i] = value;
+                value = *forward++;
+                i++;
+        }
+        forward--;
+        word[min(10, i)] = '\0';
+
+        if (i == 0)
+                return null_optional();
+        else if (i > 10)
+                return make_optional(word, 99, 2, forward);
+
+        union Optional_Token res = check_reserved_words(word);
+        if (res.nil != NULL) {
+                return res;
+        } else {
+                struct Symbol *sym_ptr = add_symbol(word, sym_table);
+                struct Token token;
+                strcpy(token.lexeme, word);
+                token.token_type = 50;
+                token.attribute.ptr = sym_ptr;
+                token.forward = forward;
+                return wrap_token(token);
+        }
 }
 
 char * ws_machine(char *forward, char *back)
--- src/machines.h
+++ src/machines.h
@@ -6,7 +6,7 @@ struct Token {
         int token_type;
         union Attribute {
                 int attribute;
-                int *ptr;
+                struct Symbol *ptr;
         } attribute;
         char *forward;
 };
@@ -16,11 +16,16 @@ union Optional_Token {
         struct Token token;
 };
 
+struct Symbol {
+        char word[11];
+        struct Symbol *ptr;
+};
+
 union Optional_Token relop_machine(char *forward, char *back);
 union Optional_Token longreal_machine(char *forward, char *back);
 union Optional_Token real_machine(char *forward, char *back);
 union Optional_Token int_machine(char *forward, char *back);
-union Optional_Token id_res_machine(char *forward, char *back);
+union Optional_Token id_res_machine(char *forward, struct Symbol *sym_table);
 char * ws_machine(char *forward, char *back);
 struct Token catchall_machine(char *forward, char *back);
 
