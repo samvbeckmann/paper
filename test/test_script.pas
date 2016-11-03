2b23668d0fca59ed55377ae1a1c44822b9c58e65
Sam Beckmann <samvbeckmann@gmail.com>
2016-11-02 22:45:01 -0500

    Added more to test script with all constructs

---
+++ test/test_script.pas
@@ -0,0 +1,9 @@
+program test(input, output);
+var num1: integer; var num2: integer;
+
+procedure testproc(in1: array [5..7] of integer; in2: real)
+    var innervar1: real; innervar2: array[7..3] of real;
+    procedure nested;
+        begin
+
+        end;
